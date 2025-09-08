d752a058925578f7b971ce97617f79a9e91b3090
tobiascrocus <tobiascrocus@hotmail.com>
2025-09-08 20:36:04 -0300

    Dejando main completamente vacía

--- BellezApp-Frontend/src/pages/Inicio.jsx
+++
@@ -1,8 +0,0 @@
-export default function Inicio() {
-  return (
-    <div>
-      <h1>Bienvenido a BellezApp</h1>
-      <p>Página de inicio estática, aquí podés poner promociones o info general.</p>
-    </div>
-  );
-}
